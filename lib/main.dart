2a1247850847e478cde190afbe21fe8cc46ad1c7
elhazent <meeannawwaf007@gmail.com>
2019-12-12 14:55:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> image = new List();
+  ScrollController _scrollController = new ScrollController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchFive();
+    _scrollController.addListener((){
+      print(_scrollController.position.pixels);
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+        // if we are at the bottom of the page
+        fetchFive();
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: image.length,
+        controller: _scrollController,
+        itemBuilder: (BuildContext context, int position){
+          return Container(
+            constraints: BoxConstraints.tightFor(height: 180),
+            child: Image.network(image[position],fit: BoxFit.fitWidth,),
+          );
+        },
+      ),
+    );
+  }
+
+  fetch() async{
+    final response = await http.get('https://dog.ceo/api/breeds/image/random');
+    if(response.statusCode == 200){
+      setState(() {
+        image.add(json.decode(response.body)['message']);
+      });
+    } else {
+      throw Exception("Failed load data");
+    }
+  }
+
+
+
+  fetchFive(){
+    for (var c = 0; c < 5; c++){
+      fetch();
+    }
+  }
+}
